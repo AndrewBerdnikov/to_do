716dd249bf76537059510985792d59bea9928375
Andrey <root@WIN-8RBIDKB4MQ8.localdomain>
2024-10-29 19:20:20 +0300

    Third commit

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "max 40" do 
+    task = Task.new(body: "1231345456786567543234543456787656787654322", user_id: 2)
+
+    assert_not task.save, "Saved the article with a title shorter than 3 characters"
+    assert_includes task.errors[:body], "is too short (minimum is 3 characters)"
+  end
 end
